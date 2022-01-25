e9af9504066b06cf17543e1c065035131ba75ca9
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2022-01-25 15:03:29 +0700

    :white_check_mark: Add SemanticVersion formatting tests

---
+++ AbbLab.SemanticVersioning.Tests/Fixtures/VersionFormatTest.cs
@@ -0,0 +1,16 @@
+﻿namespace AbbLab.SemanticVersioning.Tests
+{
+    public readonly struct VersionFormatTest
+    {
+        public string Semantic { get; }
+        public string? Format { get; }
+        public string Expected { get; }
+
+        public VersionFormatTest(string semantic, string? format, string expected)
+        {
+            Semantic = semantic;
+            Format = format;
+            Expected = expected;
+        }
+    }
+}
--- AbbLab.SemanticVersioning.Tests/SemanticVersionTests.cs
+++ AbbLab.SemanticVersioning.Tests/SemanticVersionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -11,13 +12,38 @@ namespace AbbLab.SemanticVersioning.Tests
 
         [Theory]
         [MemberData(nameof(ParseFixture))]
-        public void ParseTests(VersionInfo info)
+        public void ParseTests(VersionTest info)
         {
             Output.WriteLine($"Parsing `{info.Semantic}`.");
             bool success = SemanticVersion.TryParse(info.Semantic, out SemanticVersion? tryParseResult);
+            bool successWithOptions = SemanticVersion.TryParse(
+                info.Semantic, SemanticOptions.Strict, out SemanticVersion? tryParseResultWithOptions);
+            Assert.Equal(success, successWithOptions);
+            Assert.Equal(tryParseResult, tryParseResultWithOptions, BuildMetadataComparer.Instance!);
+
             try
             {
                 SemanticVersion parseResult = SemanticVersion.Parse(info.Semantic);
+                SemanticVersion parseResultWithOptions = SemanticVersion.Parse(info.Semantic, SemanticOptions.Strict);
+                Assert.Equal(parseResult, parseResultWithOptions, BuildMetadataComparer.Instance);
+
+                Assert.True(success, "Parse() parsed what TryParse() couldn't.");
+                if (!info.IsValid) throw new InvalidOperationException("Successfully parsed an invalid version.");
+                Assert.Equal(tryParseResult, parseResult, BuildMetadataComparer.Instance!);
+                info.Assert(tryParseResult!);
+            }
+            catch (ArgumentException)
+            {
+                Assert.False(success);
+                if (info.IsValid) throw;
+            }
+
+            // TryParse() uses StrictParse(), if options are Strict, so we're using this flag to bypass that
+            const SemanticOptions pseudoStrictMode = (SemanticOptions)int.MinValue;
+            success = SemanticVersion.TryParse(info.Semantic, pseudoStrictMode, out tryParseResult);
+            try
+            {
+                SemanticVersion parseResult = SemanticVersion.Parse(info.Semantic, pseudoStrictMode);
                 Assert.True(success, "Parse() parsed what TryParse() couldn't.");
                 if (!info.IsValid) throw new InvalidOperationException("Successfully parsed an invalid version.");
                 Assert.Equal(tryParseResult, parseResult, BuildMetadataComparer.Instance!);
@@ -32,7 +58,7 @@ namespace AbbLab.SemanticVersioning.Tests
 
         [Theory]
         [MemberData(nameof(ParseFixture))]
-        public void LooseParseTests(VersionInfo info)
+        public void LooseParseTests(VersionTest info)
         {
             Output.WriteLine($"Loosely parsing `{info.Semantic}`.");
             bool success = SemanticVersion.TryParse(info.Semantic, SemanticOptions.Loose, out SemanticVersion? tryParseResult);
@@ -51,5 +77,30 @@ namespace AbbLab.SemanticVersioning.Tests
             }
         }
 
+        [Theory]
+        [MemberData(nameof(ParseFixture))]
+        public void ToStringTests(VersionTest info)
+        {
+            if (info.IsValid)
+            {
+                Output.WriteLine($"Formatting `{info.Semantic}`.");
+                SemanticVersion version = SemanticVersion.Parse(info.Semantic);
+
+                Assert.Equal(info.Semantic, version.ToString());
+                Assert.Equal(info.Semantic, version.ToString(null));
+                Assert.Equal(info.Semantic, version.ToString("G"));
+                Assert.Equal(info.Semantic, version.ToString("g"));
+                Assert.Equal(info.Semantic, version.ToString("M.m.p-ppp+mmm"));
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(FormatFixture))]
+        public void ToStringFormatTests(VersionFormatTest info)
+        {
+            Output.WriteLine($"Formatting `{info.Semantic}` using `{info.Format}`.");
+            SemanticVersion version = SemanticVersion.Parse(info.Semantic);
+            Assert.Equal(info.Expected, version.ToString(info.Format));
+        }
     }
 }
