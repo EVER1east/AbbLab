c87929cd49ad76d9dcdc1ce1617eccdf03ddbdfa
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2022-01-25 14:02:12 +0700

    :recycle: Make Util.Arrayify generic

--- AbbLab.SemanticVersioning.Tests/Util.cs
+++ AbbLab.SemanticVersioning.Tests/Util.cs
@@ -6,7 +6,7 @@ namespace AbbLab.SemanticVersioning.Tests
     internal static class Util
     {
         // ReSharper disable once IdentifierTypo
-        public static IEnumerable<object[]> Arrayify(VersionInfo[] versions)
-            => versions.Select(static v => new object[1] { v }).ToArray();
+        public static IEnumerable<object[]> Arrayify<T>(IEnumerable<T> versions)
+            => versions.Select(static v => new object[1] { v! }).ToArray();
     }
 }
