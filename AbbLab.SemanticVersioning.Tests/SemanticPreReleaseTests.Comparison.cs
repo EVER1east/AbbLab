257f16e5092e0f546c2324c0a1f12404758aef72
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2022-01-26 23:20:08 +0700

    :white_check_mark: Add SemanticPreRelease tests
    Add comparison and constructors tests.

---
+++ AbbLab.SemanticVersioning.Tests/SemanticPreReleaseTests.Comparison.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AbbLab.SemanticVersioning.Tests
+{
+    public partial class SemanticPreReleaseTests
+    {
+        [Fact]
+        public void ComparisonTest()
+        {
+            List<SemanticPreRelease> preReleases = SortFixture.Select(SemanticPreRelease.Parse).ToList();
+            List<SemanticPreRelease> sorted = preReleases.ToList();
+            sorted.Sort();
+            Assert.Equal(preReleases, sorted); // in the same order
+
+            int count = preReleases.Count;
+            for (int i = 0; i < count; i++)
+            {
+                SemanticPreRelease me = preReleases[i];
+                for (int j = 0; j < i; j++)
+                {
+                    // greater than any of the previous identifiers
+                    SemanticPreRelease other = preReleases[j];
+
+                    Assert.True(me.CompareTo(other) > 0);
+                    Assert.True(((IComparable)me).CompareTo(other) > 0);
+                    Assert.False(me.Equals(other));
+                    Assert.False(me.Equals((object?)other));
+
+                    Assert.True(me > other);
+                    Assert.True(me >= other);
+                    Assert.False(me < other);
+                    Assert.False(me <= other);
+                    Assert.True(me != other);
+                    Assert.False(me == other);
+                }
+                for (int j = i + 1; j < count; j++)
+                {
+                    // less than any of the subsequent identifiers
+                    SemanticPreRelease other = preReleases[j];
+
+                    Assert.True(me.CompareTo(other) < 0);
+                    Assert.True(((IComparable)me).CompareTo(other) < 0);
+                    Assert.False(me.Equals(other));
+                    Assert.False(me.Equals((object?)other));
+
+                    Assert.False(me > other);
+                    Assert.False(me >= other);
+                    Assert.True(me < other);
+                    Assert.True(me <= other);
+                    Assert.True(me != other);
+                    Assert.False(me == other);
+                }
+                // equal to self
+                Assert.True(me.CompareTo(me) == 0);
+                Assert.True(((IComparable)me).CompareTo(me) == 0);
+                Assert.True(me.Equals(me));
+                Assert.True(me.Equals((object)me));
+                Assert.Equal(me.GetHashCode(), me.GetHashCode());
+
+                Assert.Throws<ArgumentException>(() => ((IComparable)me).CompareTo(0.0));
+
+#pragma warning disable CS1718 // Comparison made to same variable
+                // ReSharper disable EqualExpressionComparison
+                Assert.False(me > me);
+                Assert.True(me >= me);
+                Assert.False(me < me);
+                Assert.True(me <= me);
+                Assert.False(me != me);
+                Assert.True(me == me);
+#pragma warning restore CS1718 // Comparison made to same variable
+                // ReSharper restore EqualExpressionComparison
+
+            }
+        }
+
+        public static readonly string[] SortFixture =
+        {
+            "0",
+            "1",
+            "2",
+            "10",
+            "100",
+            "2147483647",
+            "a",
+            "alpha",
+            "b",
+            "beta",
+            "dev",
+            "nightly",
+            "rc",
+            "zzz",
+        };
+    }
+}
---
+++ AbbLab.SemanticVersioning.Tests/SemanticPreReleaseTests.Constructors.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace AbbLab.SemanticVersioning.Tests
+{
+    public partial class SemanticPreReleaseTests
+    {
+        [Fact]
+        public void ConstructorsTest()
+        {
+            int[] numbers = { 0, 1, 23, 4056, 2147483647 };
+            string[] numberStrings = Array.ConvertAll(numbers, static n => n.ToString(CultureInfo.InvariantCulture));
+
+            for (int i = 0, length = numbers.Length; i < length; i++)
+            {
+                AssertPreRelease(new SemanticPreRelease(numbers[i]), numbers[i]);
+                AssertPreRelease(numbers[i], numbers[i]); // implicit
+
+                AssertPreRelease(new SemanticPreRelease(numberStrings[i]), numbers[i]);
+                AssertPreRelease(new SemanticPreRelease(numberStrings[i], SemanticOptions.Strict), numbers[i]);
+                AssertPreRelease(new SemanticPreRelease(numberStrings[i], SemanticOptions.Loose), numbers[i]);
+                AssertPreRelease(numberStrings[i], numbers[i]); // implicit
+            }
+
+            AssertPreRelease(new SemanticPreRelease("003", SemanticOptions.Loose), 3);
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("003"));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("003", SemanticOptions.Strict));
+            Assert.Throws<ArgumentException>(static () => (SemanticPreRelease)"003");
+
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("2147483648"));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("2147483648", SemanticOptions.Strict));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("2147483648", SemanticOptions.Loose));
+            Assert.Throws<ArgumentException>(static () => (SemanticPreRelease)"2147483648");
+
+            Assert.Throws<ArgumentOutOfRangeException>(static () => new SemanticPreRelease(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(static () => (SemanticPreRelease)(-1));
+
+            string[] identifiers = { "-1", "alpha", "00alpha", "-00" };
+            for (int i = 0, length = identifiers.Length; i < length; i++)
+            {
+                string str = identifiers[i];
+                AssertPreReleaseString(new SemanticPreRelease(str), str);
+                AssertPreReleaseString(new SemanticPreRelease(str, SemanticOptions.Strict), str);
+                AssertPreReleaseString(new SemanticPreRelease(str, SemanticOptions.Loose), str);
+                AssertPreReleaseString(str, str); // implicit
+                ReadOnlySpan<char> span = str.AsSpan();
+                AssertPreReleaseSpan(new SemanticPreRelease(span), str);
+                AssertPreReleaseSpan(new SemanticPreRelease(span, SemanticOptions.Strict), str);
+                AssertPreReleaseSpan(new SemanticPreRelease(span, SemanticOptions.Loose), str);
+                AssertPreReleaseSpan(span, str); // implicit
+            }
+
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease(""));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("", SemanticOptions.Strict));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("", SemanticOptions.Loose));
+            Assert.Throws<ArgumentException>(static () => (SemanticPreRelease)"");
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("invalid$$"));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("invalid$$", SemanticOptions.Strict));
+            Assert.Throws<ArgumentException>(static () => new SemanticPreRelease("invalid$$", SemanticOptions.Loose));
+            Assert.Throws<ArgumentException>(static () => (SemanticPreRelease)"invalid$$");
+
+        }
+
+        private static void AssertPreRelease(SemanticPreRelease preRelease, int value)
+        {
+            Assert.True(preRelease.IsNumeric);
+            Assert.Equal(value, preRelease.Number);
+            Assert.Equal(value, (int)preRelease);
+            Assert.Throws<InvalidOperationException>(() => preRelease.Text);
+        }
+        private static void AssertPreReleaseString(SemanticPreRelease preRelease, string value)
+        {
+            Assert.False(preRelease.IsNumeric);
+            Assert.Equal(value, preRelease.Text);
+            Assert.Equal(value, (string)preRelease);
+            Assert.Same(value, preRelease.Text); // no need to allocate a new string
+            Assert.Same(value, (string)preRelease);
+            Assert.Throws<InvalidOperationException>(() => preRelease.Number);
+        }
+        private static void AssertPreReleaseSpan(SemanticPreRelease preRelease, string value)
+        {
+            Assert.False(preRelease.IsNumeric);
+            Assert.Equal(value, preRelease.Text);
+            Assert.Equal(value, (string)preRelease);
+            Assert.NotSame(value, preRelease.Text); // has to allocate a new string
+            Assert.NotSame(value, (string)preRelease);
+            Assert.Throws<InvalidOperationException>(() => preRelease.Number);
+        }
+
+    }
+}
