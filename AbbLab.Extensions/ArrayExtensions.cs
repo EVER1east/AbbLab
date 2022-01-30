2fcb9d63c67a56c2675ce5e464c52d4987494df2
Abbysssal <55982389+Abbysssal@users.noreply.github.com>
2022-01-30 23:57:20 +0700

    :zap: Prevent unnecessary delegate allocations

--- AbbLab.Extensions/ArrayExtensions.cs
+++ AbbLab.Extensions/ArrayExtensions.cs
@@ -23,8 +23,8 @@ namespace AbbLab.Extensions
         [Pure] public static bool Contains<T>(this T[] array, T value)
             => Array.IndexOf(array, value) is not -1;
 
-        [Pure] public static bool Exists<T>(this T[] array, [InstantHandle] Func<T, bool> predicate)
-            => Array.Exists(array, predicate.Invoke);
+        [Pure] public static bool Exists<T>(this T[] array, [InstantHandle] Predicate<T> predicate)
+            => Array.Exists(array, predicate);
         [Pure] public static bool Exists<T>(this T[] array, [InstantHandle] Func<T, int, bool> predicate)
         {
             for (int i = 0, length = array.Length; i < length; i++)
@@ -33,8 +33,8 @@ namespace AbbLab.Extensions
             return false;
         }
 
-        [Pure] public static T? Find<T>(this T[] array, [InstantHandle] Func<T, bool> predicate)
-            => Array.Find(array, predicate.Invoke);
+        [Pure] public static T? Find<T>(this T[] array, [InstantHandle] Predicate<T> predicate)
+            => Array.Find(array, predicate);
         [Pure] public static T? Find<T>(this T[] array, [InstantHandle] Func<T, int, bool> predicate)
         {
             for (int i = 0, length = array.Length; i < length; i++)
@@ -43,8 +43,8 @@ namespace AbbLab.Extensions
             return default;
         }
 
-        [Pure] public static T? FindLast<T>(this T[] array, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindLast(array, predicate.Invoke);
+        [Pure] public static T? FindLast<T>(this T[] array, [InstantHandle] Predicate<T> predicate)
+            => Array.FindLast(array, predicate);
         [Pure] public static T? FindLast<T>(this T[] array, [InstantHandle] Func<T, int, bool> predicate)
         {
             for (int i = array.Length - 1; i >= 0; i--)
@@ -53,19 +53,19 @@ namespace AbbLab.Extensions
             return default;
         }
 
-        [Pure] public static int FindIndex<T>(this T[] array, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindIndex(array, 0, array.Length, predicate.Invoke);
-        [Pure] public static int FindIndex<T>(this T[] array, int startIndex, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindIndex(array, startIndex, array.Length - startIndex, predicate.Invoke);
-        [Pure] public static int FindIndex<T>(this T[] array, int startIndex, int count, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindIndex(array, startIndex, count, predicate.Invoke);
+        [Pure] public static int FindIndex<T>(this T[] array, [InstantHandle] Predicate<T> predicate)
+            => Array.FindIndex(array, 0, array.Length, predicate);
+        [Pure] public static int FindIndex<T>(this T[] array, int startIndex, [InstantHandle] Predicate<T> predicate)
+            => Array.FindIndex(array, startIndex, array.Length - startIndex, predicate);
+        [Pure] public static int FindIndex<T>(this T[] array, int startIndex, int count, [InstantHandle] Predicate<T> predicate)
+            => Array.FindIndex(array, startIndex, count, predicate);
 
-        [Pure] public static int FindLastIndex<T>(this T[] array, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindLastIndex(array, 0, array.Length, predicate.Invoke);
-        [Pure] public static int FindLastIndex<T>(this T[] array, int startIndex, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindLastIndex(array, startIndex, array.Length - startIndex, predicate.Invoke);
-        [Pure] public static int FindLastIndex<T>(this T[] array, int startIndex, int count, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindLastIndex(array, startIndex, count, predicate.Invoke);
+        [Pure] public static int FindLastIndex<T>(this T[] array, [InstantHandle] Predicate<T> predicate)
+            => Array.FindLastIndex(array, 0, array.Length, predicate);
+        [Pure] public static int FindLastIndex<T>(this T[] array, int startIndex, [InstantHandle] Predicate<T> predicate)
+            => Array.FindLastIndex(array, startIndex, array.Length - startIndex, predicate);
+        [Pure] public static int FindLastIndex<T>(this T[] array, int startIndex, int count, [InstantHandle] Predicate<T> predicate)
+            => Array.FindLastIndex(array, startIndex, count, predicate);
 
         public static void ForEach<T>(this T[] array, [InstantHandle] Action<T> action)
             => Array.ForEach(array, action);
@@ -75,8 +75,8 @@ namespace AbbLab.Extensions
                 action(array[i], i);
         }
 
-        [Pure] public static T[] FindAll<T>(this T[] array, [InstantHandle] Func<T, bool> predicate)
-            => Array.FindAll(array, predicate.Invoke);
+        [Pure] public static T[] FindAll<T>(this T[] array, [InstantHandle] Predicate<T> predicate)
+            => Array.FindAll(array, predicate);
         [Pure] public static T[] FindAll<T>(this T[] array, [InstantHandle] Func<T, int, bool> predicate)
         {
             List<T> list = new List<T>();
@@ -86,8 +86,8 @@ namespace AbbLab.Extensions
             return list.ToArray();
         }
 
-        [Pure] public static TResult[] ConvertAll<T, TResult>(this T[] array, [InstantHandle] Func<T, TResult> converter)
-            => Array.ConvertAll(array, converter.Invoke);
+        [Pure] public static TResult[] ConvertAll<T, TResult>(this T[] array, [InstantHandle] Converter<T, TResult> converter)
+            => Array.ConvertAll(array, converter);
         [Pure] public static TResult[] ConvertAll<T, TResult>(this T[] array, [InstantHandle] Func<T, int, TResult> converter)
         {
             int length = array.Length;
@@ -106,14 +106,14 @@ namespace AbbLab.Extensions
             => Array.Sort(array);
         public static void Sort<T>(this T[] array, IComparer<T>? comparer)
             => Array.Sort(array, comparer);
-        public static void Sort<T>(this T[] array, [InstantHandle] Func<T, T, int> comparison)
-            => Array.Sort(array, Comparer<T>.Create(comparison.Invoke));
+        public static void Sort<T>(this T[] array, [InstantHandle] Comparison<T> comparison)
+            => Array.Sort(array, Comparer<T>.Create(comparison));
         public static void Sort<T>(this T[] array, int startIndex, int count)
             => Array.Sort(array, startIndex, count);
         public static void Sort<T>(this T[] array, int startIndex, int count, IComparer<T>? comparer)
             => Array.Sort(array, startIndex, count, comparer);
-        public static void Sort<T>(this T[] array, int startIndex, int count, [InstantHandle] Func<T, T, int> comparison)
-            => Array.Sort(array, startIndex, count, Comparer<T>.Create(comparison.Invoke));
+        public static void Sort<T>(this T[] array, int startIndex, int count, [InstantHandle] Comparison<T> comparison)
+            => Array.Sort(array, startIndex, count, Comparer<T>.Create(comparison));
 
         public static TResult[] Cast<TResult>(this Array array)
         {
